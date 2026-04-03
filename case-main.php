ad4a5959673f129041baef19b36d77c979f8c79d
lianet <jack.tolmak@yandex.ru>
2026-04-03 22:31:38 +0300

    add case header block

--- case-main.php
+++ case-main.php
@@ -10,20 +10,31 @@ Template Post Type: case
 
 <?php require_once(TEMPLATE_PATH . '_breadcrumbs.php'); ?>
 
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <article
-            id="post-<?php the_ID(); ?>"
-            <?php body_class('case-page'); ?>>
-
-            <section class="case-hero">
-                <div class="container">
-                    <h1 class="case-hero__title"><?php the_title(); ?></h1>
-                </div>
-            </section>
-
-        </article>
-
-<?php endwhile;
-endif; ?>
+<section class="case">
+    <div class="case__container container">
+        <div class="case__main">
+            <div class="case__categories">
+                <a href="" class="case__category">Category</a>
+                <a href="" class="case__category">Category</a>
+                <a href="" class="case__category">Category</a>
+            </div>
+            <h1 class="case__title title-sm">Case Headline <br> Key Result + Client Name</h1>
+            <p class="case__description">Short case description – 2-3 sentences. What was done, for whom, main outcome. Collpases with +- toggle on mobile</p>
+            <div class="case__actions">
+                <a href="" class="case__btn btn btn-primary">Get Similar Results</a>
+                <a href="" class="case__btn btn btn-grey">See How We Did It</a>
+            </div>
+        </div>
+        <picture class="case__image">
+            <source
+                srcset="<?php echo IMG_PATH . '/cases/case_study-1.webp' ?>"
+                type="image/webp">
+            <img
+                src="<?php echo IMG_PATH . '/cases/case_study-1.jpg' ?>"
+                alt="case image"
+                class="cover-image">
+        </picture>
+    </div>
+</section>
 
 <?php get_footer(); ?>
