f2e09150322acb669d6a9e39b9023a78021fd4a2
Roma <rstrah4@gmail.com>
2026-04-08 22:06:10 +0300

    add blog + in-develop template

---
+++ blog-main.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Template Name: Main Blog
+Template Post Type: blog
+*/
+?>
+<?php $page_id = $post->ID; ?>
+
+<?php get_header(); ?>
+
+<?php require_once(TEMPLATE_PATH . '_breadcrumbs.php'); ?>
+
+
+<?php get_footer(); ?>
---
+++ in-develop.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Template Name: In develop
+*/
+?>
+<?php $page_id = $post->ID; ?>
+
+<?php get_header(); ?>
+<?php the_content(); ?>
+<?php get_footer(); ?>
