8b75c1be708196e21b9591598f3a4916ca30a6d4
lianet <jack.tolmak@yandex.ru>
2026-04-24 19:33:55 +0300

    fix case heading sefction

--- acf-components/block-stive-case-header.php
+++ acf-components/block-stive-case-header.php
@@ -17,18 +17,23 @@ $case_image = get_field('case_image'); //img
             <h1 class="heading__title title-xs"><?php echo $case_title; ?></h1>
             <p class="heading__description"><?php echo $case_description; ?></p>
             <div class="heading__actions">
-                <a data-fancybox href="#get-proposal" class="heading__btn btn btn-primary">Get Similar Results</a><?php if ($case_btn_secondary) {
+                <a data-fancybox href="#get-proposal" class="heading__btn btn btn-primary">Get Similar Results</a>
+                <?php if ($case_btn_secondary) {
                     $link_url = $case_btn_secondary['url'];
                     $link_title = $case_btn_secondary['title'];
                     $link_target = $case_btn_secondary['target'] ? $case_btn_secondary['target'] : '_self';
-                ?><a href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>"
+                ?>
+                    <a href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>"
                         class="heading__btn btn btn-grey"><?php echo esc_html($link_title); ?></a>
-                <?php } ?></div></div>
+                <?php } ?>
+            </div>
+        </div>
         <picture class="heading__image">
             <?php if (!empty($case_image)) { ?>
                 <img
                     src="<?php echo esc_url($case_image['url']); ?>"
-                    alt="<?php echo esc_attr($case_image['alt']); ?>">
+                    alt="<?php echo esc_attr($case_image['alt']); ?>"
+                    class="cover-image">
             <?php } ?>
         </picture>
     </div>
