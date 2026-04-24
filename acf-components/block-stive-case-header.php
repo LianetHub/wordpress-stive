81009b08d215186d238572d4475c6a49554b0924
Roma <rstrah4@gmail.com>
2026-04-24 11:11:55 +0300

    fix fantom <p>

--- acf-components/block-stive-case-header.php
+++ acf-components/block-stive-case-header.php
@@ -1,8 +1,8 @@
 <?php
-$case_title = get_field('case_title'); //text
-$case_description = get_field('case_description'); //text
-$case_btn_secondary = get_field('case_btn_secondary'); //link
-$case_image = get_field('case_image'); //img
+$case_title = get_field('case_title');
+$case_description = get_field('case_description');
+$case_btn_secondary = get_field('case_btn_secondary');
+$case_image = get_field('case_image');
 ?>
 <section class="heading">
     <div class="heading__container container">
@@ -10,26 +10,21 @@ $case_image = get_field('case_image'); //img
             <div class="heading__categories">
                 <?php echo display_category_and_tag_terms($post_id = get_the_ID(), $taxonomy = 'case-list', $tag = 'a', $class = 'heading__categories label-badge'); ?>
             </div>
-            <h1 class="heading__title title-sm"><?php echo $case_title; ?></h1>
-            <p class="heading__description"><?php echo $case_description; ?></p>
+            <h1 class="heading__title title-sm"><?php echo esc_html($case_title); ?></h1>
+            <p class="heading__description"><?php echo esc_html($case_description); ?></p>
             <div class="heading__actions">
                 <a data-fancybox href="#get-proposal" class="heading__btn btn btn-primary">Get Similar Results</a>
-                <?php if ($case_btn_secondary) {
+                <?php if ($case_btn_secondary) :
                     $link_url = $case_btn_secondary['url'];
                     $link_title = $case_btn_secondary['title'];
                     $link_target = $case_btn_secondary['target'] ? $case_btn_secondary['target'] : '_self';
                     ?>
-                    <a href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>"
-                       class="heading__btn btn btn-grey"><?php echo esc_html($link_title); ?></a>
-                <?php } ?>
-            </div>
-        </div>
+                    <a href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>" class="heading__btn btn btn-grey"><?php echo esc_html($link_title); ?></a>
+                <?php endif; ?></div></div>
         <picture class="heading__image">
-            <?php if (!empty($case_image)) { ?>
-                <img
-                        src="<?php echo esc_url($case_image['url']); ?>"
-                        alt="<?php echo esc_attr($case_image['alt']); ?>">
-            <?php } ?>
+            <?php if (!empty($case_image)) : ?>
+                <img src="<?php echo esc_url($case_image['url']); ?>" alt="<?php echo esc_attr($case_image['alt']); ?>">
+            <?php endif; ?>
         </picture>
     </div>
 </section>
