14445bb7f31fff31545ca6cdd442b2b595e4953b
lianet <jack.tolmak@yandex.ru>
2026-04-25 14:49:44 +0300

    fix css bugs

--- acf-components/block-stive-case-article.php
+++ acf-components/block-stive-case-article.php
@@ -2,7 +2,7 @@
 $case_article_title = get_field('case_article_title'); //text
 $case_article_block = get_field('case_article_block'); //wysiwyg
 ?>
-<article class="article">
+<article class="article article--case">
     <div class="article__container container typography-block">
         <h2><?php echo $case_article_title; ?></h2>
         <?php echo $case_article_block; ?>
