b0bf299a7dcd86c7b042808781438bc7b7c1e303
Roma <rstrah4@gmail.com>
2026-05-05 19:09:02 +0300

    fix

--- landing/landing-1/footer-landing-1.php
+++ landing/landing-1/footer-landing-1.php
@@ -4,4 +4,9 @@ declare(strict_types=1);
 <?php require_once(TEMPLATE_PATH . '_modal-get-proposal.php'); ?>
 <?php wp_footer(); ?>
 </body>
+<script type="text/javascript">
+    document.addEventListener("DOMContentLoaded", function() {
+        Fancybox.bind("[data-fancybox]", {});
+    })
+</script>
 </html>
--- landing/landing-2/footer-landing-2.php
+++ landing/landing-2/footer-landing-2.php
@@ -6,4 +6,9 @@ defined('ABSPATH') || exit;
 <?php require_once(TEMPLATE_PATH . '_modal-get-proposal.php'); ?>
 <?php wp_footer(); ?>
 </body>
+<script type="text/javascript">
+    document.addEventListener("DOMContentLoaded", function() {
+        Fancybox.bind("[data-fancybox]", {});
+    })
+</script>
 </html>
