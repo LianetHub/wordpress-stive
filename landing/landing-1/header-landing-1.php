f14db33da1e3e79e861c93347cdbfda25bed04a5
Roma <rstrah4@gmail.com>
2026-05-06 16:30:52 +0300

    add title to landing

--- landing/landing-1/header-landing-1.php
+++ landing/landing-1/header-landing-1.php
@@ -6,6 +6,7 @@ declare(strict_types=1);
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php the_title(); ?></title>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
--- landing/landing-3/header-landing-3.php
+++ landing/landing-3/header-landing-3.php
@@ -7,6 +7,7 @@ defined('ABSPATH') || exit;
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php the_title(); ?></title>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
