b6b4d5a7fa22e58295d1be9d67204fa2136a9ce9
Roma <rstrah4@gmail.com>
2026-05-07 13:18:53 +0300

    fix landing

--- landing/landing-1/header-landing-1.php
+++ landing/landing-1/header-landing-1.php
@@ -5,7 +5,17 @@ declare(strict_types=1);
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="format-detection" content="telephone=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- favicon -->
+    <link rel="icon" type="image/png" href="<?php echo ASSETS_PATH ?>/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/svg+xml" href="<?php echo ASSETS_PATH ?>/favicon.svg">
+    <link rel="shortcut icon" href="<?php echo ASSETS_PATH ?>/favicon.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo ASSETS_PATH ?>/apple-touch-icon.png">
+    <meta name="apple-mobile-web-app-title" content="Stive">
+    <link rel="manifest" href="<?php echo ASSETS_PATH ?>/site.webmanifest">
+    <!-- favicon -->
     <title><?php the_title(); ?></title>
     <?php wp_head(); ?>
 </head>
--- landing/landing-3/header-landing-3.php
+++ landing/landing-3/header-landing-3.php
@@ -8,9 +8,18 @@ $assetsUri = get_template_directory_uri() . '/landing/landing-3/assets/img';
 <head>
 <meta charset="utf-8"/>
 <title><?php the_title(); ?></title>
-<meta name="viewport" content="width=device-width, initial-scale=1"/>
-<link rel="icon" href="<?php echo esc_url($assetsUri . '/favicon.png'); ?>" type="image/png"/>
-<link rel="apple-touch-icon" href="<?php echo esc_url($assetsUri . '/apple-touch-icon.png'); ?>"/>
+<meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="format-detection" content="telephone=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- favicon -->
+    <link rel="icon" type="image/png" href="<?php echo ASSETS_PATH ?>/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/svg+xml" href="<?php echo ASSETS_PATH ?>/favicon.svg">
+    <link rel="shortcut icon" href="<?php echo ASSETS_PATH ?>/favicon.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo ASSETS_PATH ?>/apple-touch-icon.png">
+    <meta name="apple-mobile-web-app-title" content="Stive">
+    <link rel="manifest" href="<?php echo ASSETS_PATH ?>/site.webmanifest">
+    <!-- favicon -->
 <link href="https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&family=Onest:wght@400;500;600;700;800&display=swap" rel="stylesheet"/>
 <?php wp_head(); ?>
 </head>
