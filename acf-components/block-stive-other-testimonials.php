9f5322e21064cf33b56486cc3caa572ebe55ad9b
lianet <jack.tolmak@yandex.ru>
2026-05-16 13:56:15 +0300

    add title to testimonials block

--- acf-components/block-stive-other-testimonials.php
+++ acf-components/block-stive-other-testimonials.php
@@ -6,11 +6,38 @@ if (!isset($through_testimonials_sliders) && function_exists('get_field')) {
         $through_testimonials_sliders = get_field('through_testimonials_sliders', 'option');
     }
 }
+
+if (!isset($through_testimonials_section_title) && function_exists('get_field')) {
+    $through_testimonials_section_title = get_field('other_testimonials');
+
+    if (!is_string($through_testimonials_section_title) || trim($through_testimonials_section_title) === '') {
+        $through_testimonials_section_title = get_field('other_testimonials', 'option');
+    }
+
+    $through_testimonials_section_title = is_string($through_testimonials_section_title)
+        ? trim($through_testimonials_section_title)
+        : '';
+} elseif (!isset($through_testimonials_section_title)) {
+    $through_testimonials_section_title = '';
+} else {
+    $through_testimonials_section_title = trim((string) $through_testimonials_section_title);
+}
+
+$has_testimonials_title = $through_testimonials_section_title !== '';
+$testimonials_wrapper_tag = $has_testimonials_title ? 'section' : 'div';
+
+$testimonials_title_class = 'testimonials__title title-xs gradient-text';
+if (defined('STIVE_SERVICE_POST_TYPE') && is_singular(STIVE_SERVICE_POST_TYPE)) {
+    $testimonials_title_class = 'testimonials__title title-xs gradient-text';
+}
 ?>
 
 <?php if (!empty($through_testimonials_sliders)) : ?>
-    <div class="testimonials" id="testimonials">
+    <<?php echo esc_attr($testimonials_wrapper_tag); ?> class="testimonials" id="testimonials">
         <div class="container">
+            <?php if ($has_testimonials_title) : ?>
+                <h2 class="<?php echo esc_attr($testimonials_title_class); ?>"><?php echo esc_html($through_testimonials_section_title); ?></h2>
+            <?php endif; ?>
             <div class="testimonials__slider swiper">
                 <ul class="swiper-wrapper">
                     <?php foreach ($through_testimonials_sliders as $testimonial_item) : ?>
@@ -40,5 +67,5 @@ if (!isset($through_testimonials_sliders) && function_exists('get_field')) {
                 </ul>
             </div>
         </div>
-    </div>
+    </<?php echo esc_attr($testimonials_wrapper_tag); ?>>
 <?php endif; ?>
